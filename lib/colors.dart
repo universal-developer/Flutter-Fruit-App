157af65ca62bf9afc73e2cb5464725954eaa3dbf
universal developer <artyshjidinaggg@gmail.com>
2022-05-29 22:02:39 +0300

    Initial Commit

--- lib/colors.dart
+++ lib/colors.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 Color backgroundColor = const Color(0xff1E1E1E);
 Color greyColor = const Color(0xff2C2C2C);
+Color orangeColor = const Color(0xffF9A94A);
+Color lightGreyColor = Color.fromARGB(255, 235, 235, 245);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:fruit_app/colors.dart';
@@ -33,7 +35,14 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: [
           const SizedBox(height: 30),
-          _saleCard(),
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50), color: greyColor),
+              width: 350,
+              child: _saleCard(),
+            ),
+          ),
         ],
       ),
     );
@@ -41,18 +50,90 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 Widget _saleCard() {
-  return Center(
-    child: Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10), color: greyColor),
-      width: 350,
-      child: Column(
-        children: const [
-          Image(
+  return Stack(
+    children: [
+      Positioned(
+        //<-- SEE HERE
+        left: 80,
+        bottom: 0,
+        child: Container(
+          child: Image(
             image: AssetImage('assets/pineapple.png'),
           ),
-        ],
+        ),
       ),
-    ),
+      Container(
+        width: 270,
+        height: 300,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 4.0, sigmaY: 4.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 50),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'O F F E R',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Color(0xffDBA683),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'Discount up to 40% Off',
+                  style: TextStyle(
+                    fontSize: 19,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'In honor of World Health Day We’d like to give you this amazing offers.',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Color.fromARGB(255, 235, 235, 245),
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(left: 20, bottom: 20),
+                width: 270,
+                height: 100,
+                child: ElevatedButton(
+                  child: const Text(
+                    'View Offers',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xffF9A94A),
+                    textStyle: const TextStyle(
+                        fontSize: 17, fontWeight: FontWeight.bold),
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ],
   );
 }
